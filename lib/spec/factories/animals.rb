2296d5246fbb428022268970b46d742f4dcc2b95
Michael <mmarti26@alumni.nd.edu>
2016-08-13 14:25:16 +0100

    added first animal specs

---
+++ lib/spec/factories/animals.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :animal do
+    name "cat"
+    legcount 4
+    lifespan 7
+    is_endangered false
+  end
+end
---
+++ lib/spec/models/animal_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+require 'spec_helper'
+
+describe Animal do
+  before { @animal = FactoryGirl.build(:animal) }
+
+  subject { @animal }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:leg_count) }
+  it { should respond_to(:lifespan) }
+  it { should respond_to (:is_endangered)}
+
+  it { should be_valid }
+end
